2f8d7964c55760ad1027121e1920dbaf98bd3ad3
Brett Hodge <brettchodge@gmail.com>
2023-11-27 18:26:15 +0100

    fix destry action, add form styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :friends
-  
+
   get 'home/about'
   root 'home#index'
+  delete 'friends/:id', to: 'friends#destroy', as: "delete_friend"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
